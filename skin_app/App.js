c590ac1bd431a61157cd8d3ef5ddd688eeef722f
Pablo Ortiz <poloritz4@hotmail.com>
2017-12-05 15:11:20 -0500

    Create First Screen, not super pretty yet

--- skin_app/App.js
+++ skin_app/App.js
@@ -1,23 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, Image, StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  _on_press_enter(){
+      Alert.alert('Entering...!')
+      this.
+  }
+  _on_press_profile(){
+      Alert.alert('Sorry, VIP only...!')
+  }
+    constructor(props) {
+        super(props);
+        this.state = {text: ''}
+      }
   render() {
+    let pic = {
+      uri: 'https://image.freepik.com/free-icon/health-care_318-134428.jpg'
+    };
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{padding: 70}}>
+          <Text style={styles.titleText}> Skin Classification App</Text>
+        <Text>Log In:</Text>
+        <TextInput
+          style={{height: 50}}
+          placeholder="Username"
+          onChangeText={(text) => this.setState({text})}
+        />
+          <TextInput
+          style={{height: 50}}
+          placeholder="Password"
+          onChangeText={(text) => this.setState({text})}
+        />
+          <Button
+              onPress = {this._on_press_enter}
+              title="Enter"
+           />
+          <Button
+              onPress = {this._on_press_profile}
+              title="Create New Profile"
+           />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  baseText: {
+    fontFamily: 'Cochin',
+  },
+  titleText: {
+    fontFamily: 'Baskerville',
+    fontSize: 50,
+    fontWeight: 'bold',
+    height: 200,
   },
 });
