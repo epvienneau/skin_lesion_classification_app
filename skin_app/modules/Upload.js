c6ebbdbcd1ee40555ce888b326402f6b6f696922
Julia <julia.l.ross@duke.edu>
2017-12-09 16:45:22 -0500

    who really knows why that didnt work on Emelinas comp but here we go

--- skin_app/modules/Upload.js
+++ skin_app/modules/Upload.js
@@ -34,6 +34,31 @@ export default class Upload extends React.Component {
 	  value = {this.state.diam}
 	/>
 	<Text style={styles.smallerText}> Date Image was Captured: </Text>
+	<DatePicker
+           style={{width: 200}}
+           date={this.state.date}
+           mode="date"
+           placeholder="select date"
+           format="YYYY-MM-DD"
+           minDate="1900-05-01"
+           maxDate="2018-06-01"
+           confirmBtnText="Confirm"
+           cancelBtnText="Cancel"
+           customStyles={{
+           dateIcon: {
+           position: 'absolute',
+           left: 0,
+           top: 4,
+           marginLeft: 0
+           },
+           dateInput: {
+           marginLeft: 36
+           }
+          // ... You can check the source to find the other keys.
+          }}
+          onDateChange={(date) => {this.setState({date: date})}}
+        />
+
 	<Text style={styles.smallerText}> Lesion Tag </Text>
 	<TextInput
 	  style={{height: 40, borderColor: 'gray', borderWidth: 1, margin:5}}
