2eda5079abc4ef5e52d92f7af6e5509d4d913d4f
epvienneau <epv4@duke.edu>
2017-12-11 23:15:44 -0500

    upon clicking login button, screen prop is changed so app renders home screen

--- skin_web_app/src/StartScreen.js
+++ skin_web_app/src/StartScreen.js
@@ -1,65 +1,49 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
-import logo from './logo.svg'
-import CreateProfile from './CreateProfile';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
+
 class StartScreen extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {username: '',password: ''}
-	}
-	changeUsername = (event) => {
-    		this.setState({
-      		username: event.target.value,
-    		});
-	}
-	changePassword = (event) => {
-    		this.setState({
-      		username: event.target.value,
-    		});}
-	checkUserExists = (event) => 
-		{
-		
-		RouteToCreate
-	
-	}
+  constructor(props) {
+    super(props);
+    this.state = {username: '',password: ''}
+  }
+  changeUsername = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+  }
+  changePassword = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+  }
+  
+  render(){
+    return(
+      <div>
+        <MuiThemeProvider>
+          <TextField
+            id="text-field-controlled"
+            value={this.state.username}
+            onChange={this.changeUsername}/>
+	    <br/>
+	    <TextField
+            id="text-field-controlled"
+            value={this.state.password}
+            onChange={this.changePassword}/>
+	    <br/>
+	    <RaisedButton
+	    label="Log In"
+      	    labelPosition="before"
+      	    style={styles.button}
+	    onClick={() => {this.props.onScreenChange('homescreen')}}/>
+	</MuiThemeProvider>
+      </div>
+    )
+  }
+}
 
-
-	render(){
-	
-		return(
-			<div>
-				<MuiThemeProvider>
-					 <TextField
-          				id="text-field-controlled"
-          				value={this.state.username}
-          				onChange={this.changeUsername}
-        				/><br/>
-					 <TextField
-          				id="text-field-controlled"
-          				value={this.state.password}
-          				onChange={this.changePassword}
-        				/><br/>
-					<RaisedButton
-					label="Log In"
-      					labelPosition="before"
-      					style={styles.button}
-					onClick={this.checkUserExists}
-					/>
-					
-				</MuiThemeProvider>
-			</div>
-
-		)
-	
-	}}
 const styles = {
   button: {
     margin: 12,
@@ -75,24 +59,5 @@ const styles = {
     opacity: 0,
   },
 };
-
-
-	const RouteToCreate = () =>{
-		<Router>
-    			<div>
-      			<ul>
-        		<li><Link to="/CP">CreateProfile</Link></li>
-			</ul>
-			<hr/>
-			<Route exact path="/CP" component={CreateProfile}/>
-			</div>
-		</Router>
-	}
-	const Home = () => {
-  		<div>
-   	 		<CreateProfile/>
-  		</div>
-	}
-
 
 export default StartScreen;
